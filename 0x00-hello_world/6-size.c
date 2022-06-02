e46a8620f9b55b8df873ff4259354fcd637d188f
Kwaku Adusei Okyere <dobridobrev900@gmail.com>
2022-06-02 10:10:51 +0000

    size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - main block
+ * Return: 0
+ */
+int main(void)
+{
+	int integer;
+	char character;
+	long int longman;
+	long long int longerman;
+	float titanic;
+	printf("Size of a char: %lu byte(s)\n",sizeof(character));
+	printf("Size of an int: %lu byte(s)\n",sizeof(integer));
+	printf("Size of a long int: %lu byte(s)\n",sizeof(longman));
+	printf("Size of a long long int: %lu byte(s)\n",sizeof(longerman));
+	printf("Size of a float: %lu byte(s)\n",sizeof(titanic));
+	return (0);
+}
