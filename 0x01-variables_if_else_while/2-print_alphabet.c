a65b60f72c3b9676d4f8c5bbbbfd3775fb07baa7
Kwaku Adusei Okyere <dobridobrev900@gmail.com>
2022-06-03 14:58:50 +0000

    alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - main block
+ * Return: 0
+ */
+int main(void)
+{
+	char alphabets[26] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
+
+	for (i = 0; i < 26; i++)
+	{
+		putchar(alphabets[i]);
+	}
+	putchar('\n');
+
+	return (0);
+}
