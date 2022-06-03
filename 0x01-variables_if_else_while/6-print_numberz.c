684a6b67be73ccb2faf3e71e5105ef968528e4aa
Kwaku Adusei Okyere <dobridobrev900@gmail.com>
2022-06-03 16:10:26 +0000

    numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - main block
+ * Return: 0
+ */
+int main(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		putchar(i % 10 + '0');
+		i++;
+	}
+	putchar('\n');
+
+	return (0);
+}
