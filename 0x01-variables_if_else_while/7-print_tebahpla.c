3eb80c9d29010a6f3b10a62365398d56bb9d846d
Kwaku Adusei Okyere <dobridobrev900@gmail.com>
2022-06-04 07:10:29 +0000

    tebahlpla

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - main block
+ * Return: 0
+ */
+int main(void)
+{
+	char alpha = 'z';
+
+	while (alpha >= 'a')
+	{
+		putchar(alpha);
+		alpha--;
+	}
+	putchar('\n');
+
+	return (0);
+}
