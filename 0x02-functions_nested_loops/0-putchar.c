ab3561ee413de0e75b8e95fa3ab3e9eecec1daec
Kwaku Adusei Okyere <dobridobrev900@gmail.com>
2022-06-07 13:49:16 +0000

    _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,21 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
  * main - main block
+ * Description: outputs `putchar` using _putchar function
  * Return: 0
  */
+
 int main(void)
 {
-	printf("%s\n", "_putchar");
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 	return (0);
 }
