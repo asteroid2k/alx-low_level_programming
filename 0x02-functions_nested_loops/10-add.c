906ceeb7ede4102d44b3a0a102ac4adf37887175
Kwaku Adusei Okyere <dobridobrev900@gmail.com>
2022-06-07 16:41:35 +0000

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - add numbers
+ * @x: integer input
+ * @y: integer input
+ * Description: function that adds two integers and returns the result
+ */
+
+int add(int x, int y)
+{
+	return (x + y);
+}
