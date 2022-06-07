6b9c2b4073662d599c3c68c09be30820a3cfb6cb
Kwaku Adusei Okyere <dobridobrev900@gmail.com>
2022-06-07 13:54:34 +0000

    alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - main block
+ * Description: prints alphabets
+ * Return: 0
+ */
+
+int main(void)
+{
+	char al = 'a';
+
+	while (al <= 'z')
+	{
+		_putchar(al);
+		al++;
+	}
+		_putchar('\n');
+	return (0);
+}
