e0311ea470eba40b0417ac7d8dab72079f35c1fa
Kwaku Adusei Okyere <dobridobrev900@gmail.com>
2022-06-09 12:06:01 +0000

    mul

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - multiply numbers
+ * @a: number input
+ * @b: number input
+ * Description: finds product of two numbers
+ * Return: Returns product of a and b
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
