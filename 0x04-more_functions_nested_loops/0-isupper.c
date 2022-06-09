6c846d7db570b462ad8fd22e732fb138620eca00
Kwaku Adusei Okyere <dobridobrev900@gmail.com>
2022-06-09 11:56:53 +0000

    isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _isupper - checks case of character
+ * @c: character input
+ * Description: checks for uppercase character
+ * Return: Returns 1 if c is uppercase, or 0 otherwise
+ */
+
+int _isupper(int c)
+{
+	return (c >= 65 && c <= 89);
+}
