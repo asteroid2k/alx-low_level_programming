667f96fc6ca2c17f4f676bc11a1b385395ed32b6
Kwaku Adusei Okyere <dobridobrev900@gmail.com>
2022-06-09 20:37:21 +0000

    fizzbuzzy

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+/**
+ * main - main
+ * Description: fizzbuzz
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("Fizz Buzz");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz");
+		}
+		else if (i % 35 == 0)
+		{
+			printf("Buzz");
+		}
+		else
+		{
+			printf("%d", i);
+		}
+
+		if (i < 100)
+		{
+			printf(" ");
+		}
+	}
+	printf("\n");
+	return (0);
+}
