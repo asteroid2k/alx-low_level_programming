4fe1a3475a3299cf5c7548e24aa59d0df3766679
Kwaku Adusei Okyere <dobridobrev900@gmail.com>
2022-06-09 20:47:42 +0000

    triangle

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * print_triangle - print a triangle
+ * @size: size of triangle
+ * Description: prints a triangle
+ */
+void print_triangle(int size)
+{
+	int x, y, z;
+
+	x = 0;
+	y = size - 1;
+	while (x < size)
+	{
+		y = size - 1 - x;
+		z = x + 1;
+		while (y > 0)
+		{
+			_putchar(' ');
+			y--;
+		}
+		while (z > 0)
+		{
+			_putchar('#');
+			z--;
+		}
+		_putchar('\n');
+		x++;
+	}
+
+	if (size <= 0)
+		_putchar('\n');
+}
