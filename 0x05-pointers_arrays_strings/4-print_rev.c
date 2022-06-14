d0568535bc14751f640a9459a920edcfcb4510df
Kwaku Adusei Okyere <dobridobrev900@gmail.com>
2022-06-14 03:34:34 +0000

    reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * print_rev - reverse string
+ * Description: prints a string, in reverse, followed by a new line
+ * @s: string pointer
+ */
+void print_rev(char *s)
+{
+	int i = strlen(s);
+
+	while (i >= 0)
+	{
+		_putchar(*(s + i));
+		i--;
+	}
+	_putchar('\n');
+}
