80b08810a8a019a90aaf7b1c6f749f8e35aa6974
Kwaku Adusei Okyere <dobridobrev900@gmail.com>
2022-06-18 06:51:02 +0000

    strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -12,13 +12,13 @@ char *_strcat(char *dest, char *src)
 	int i, c;
 
 	i = strlen(dest);
-	
+
 		for (c = 0; src[c] != '\0'; c++)
 		{
 			dest[i] = src[c];
 			i++;
 		}
-	
+
 
 	dest[i] = '\0';
 	return (dest);
