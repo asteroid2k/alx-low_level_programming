2be83339804e02e6d84514cfffaee554cf71bf10
Kwaku Adusei Okyere <dobridobrev900@gmail.com>
2022-06-18 06:40:51 +0000

    strcat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * _strcat - concats strings
+ * @dest: target string
+ * @src: string to append
+ * Return: concatenated string
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int i, c;
+
+	for (i = 0; dest[i] != '\0'; i++)
+	{
+		for (c = 0; src[c] != '\0'; c++)
+		{
+			dest[i] = src[c];
+		}
+		i++;
+	}
+
+	dest[i] = '\0';
+	return (dest);
+}
