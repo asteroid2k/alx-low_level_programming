fdbdd63c17017aa7a2221eb6ab45f6d96610d4a7
Kwaku Adusei Okyere <dobridobrev900@gmail.com>
2022-06-24 04:01:11 +0000

    advanced

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,6 +1,8 @@
+int wildcmp(char *s1, char *s2);
 int _pow_recursion(int x, int y);
 int is_prime_number(int n);
 int factorial(int n);
+int is_palindrome(char *s);
 int _strlen_recursion(char *s);
 void _print_rev_recursion(char *s);
 void _puts_recursion(char *s);
