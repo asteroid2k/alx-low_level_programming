e75c28c521926c9b12ae40254e0d44dbe5d66015
Kwaku Adusei Okyere <dobridobrev900@gmail.com>
2022-09-08 05:01:31 +0000

    dynamic libs

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,57 @@
+/**
+ * add - add two integers
+ *
+ * @a: int 1
+ * @b: int 2
+ * Return: sum of two integers
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - subtract two integers
+ *
+ * @a: int 1
+ * @b: int 2
+ * Return: sum of two integers
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul - multiply two integers
+ *
+ * @a: int 1
+ * @b: int 2
+ * Return: sum of two integers
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * div - div two integers
+ * @a: int 1
+ * @b: int 2
+ * Return: sum of two integers
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * mod - mod two integers
+ * @a: int 1
+ * @b: int 2
+ * Return: sum of two integers
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
